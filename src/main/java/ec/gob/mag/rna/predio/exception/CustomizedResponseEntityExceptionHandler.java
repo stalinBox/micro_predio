d7fbe51cd11c1b60343fd5025b9e8432bd8290fe
Darwin Stalin Ramirez Supe <dramirez@mag.gob.ec>
2019-11-07 15:51:33 -0500

    aplication.properties

--- src/main/java/ec/gob/mag/rna/predio/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/ec/gob/mag/rna/predio/exception/CustomizedResponseEntityExceptionHandler.java
@@ -38,155 +38,120 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import ec.gob.mag.rna.predio.util.Util;
 
 import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
+
 @ControllerAdvice
 @RestController
-public class CustomizedResponseEntityExceptionHandler 
-extends ResponseEntityExceptionHandler {
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@Autowired
 	private MessageSource messageSource;
 	private final static Logger LOGGER = LoggerFactory.getLogger(CustomizedResponseEntityExceptionHandler.class);
+
 	@ExceptionHandler(Exception.class)
-	public final ResponseEntity<Object>
-				 handleAllException(Exception ex, WebRequest request ){
-		ExceptionResponse exceptionResponse=
-				new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR,new Date(), ex.getMessage(), request.getDescription(false),null);
+	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR, new Date(),
+				ex.getMessage(), request.getDescription(false), null);
 		return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-		
+
 	}
+
 	@ExceptionHandler(DataIntegrityViolationException.class)
-	public final ResponseEntity<Object>
-				 handleConstraintDataIntegrityViolationException(Exception ex, WebRequest request ){
-		ExceptionResponse exceptionResponse=
-				new ExceptionResponse(HttpStatus.BAD_REQUEST,new Date(), 
-						messageSource.getMessage("error.constraint_validation.message", null, LocaleContextHolder.getLocale()), 
-						                          request.getDescription(false),null );
-		
-		return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);	
-	}
-	
-	@ExceptionHandler(PersistenceException.class)
-	public final ResponseEntity<Object>
-				 handlePersistenceException(Exception ex, WebRequest request ){
-		ExceptionResponse exceptionResponse=
-				new ExceptionResponse(HttpStatus.NOT_FOUND,new Date(), "Error en campos de claves foraneas", request.getDescription(false),null);
-		return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
-		
-	}
-	
+	public final ResponseEntity<Object> handleConstraintDataIntegrityViolationException(Exception ex,
+			WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.BAD_REQUEST, new Date(),
+				messageSource.getMessage("error.constraint_validation.message", null, LocaleContextHolder.getLocale()),
+				request.getDescription(false), null);
 
-	
-	@ExceptionHandler(IllegalArgumentException.class)
-	public final ResponseEntity<Object>
-				 handleIllegalArgumentException(Exception ex, WebRequest request ){
-		ExceptionResponse exceptionResponse=
-				new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR,new Date(), ex.getMessage(), request.getDescription(false),null);
-		return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-		
-	}
-	@ExceptionHandler(PredioNotFoundException.class)
-	public final ResponseEntity<Object>
-				 handleOfficerNotFoundException(Exception ex, WebRequest request ){
-		ExceptionResponse exceptionResponse=
-				new ExceptionResponse(HttpStatus.NOT_FOUND,new Date(), ex.getMessage(), request.getDescription(false),null);
-		return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
-		
-	}
-	
-
-	 @ExceptionHandler(com.fasterxml.jackson.core.JsonParseException.class)
-
-		public final ResponseEntity<Object>
-		 handleJsonParseException(Exception ex, WebRequest request ){
-			ExceptionResponse exceptionResponse=
-					new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR,new Date(), 
-							messageSource.getMessage("error.json_parse.exception.message",null, 
-									   LocaleContextHolder.getLocale()), ex.getMessage(),null);
-			return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-			
-		}
-	 /*@ExceptionHandler(HttpMessageNotReadableException.class)
-	 public  ResponseEntity<Object> handleJsonException(WebRequest response, Exception ex) {
-			ExceptionResponse exceptionResponse=
-					new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),new Date(), 
-							messageSource.getMessage("error.json_parse.exception.message",null, 
-									   LocaleContextHolder.getLocale()), ex.getMessage(),null);
-			return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-
-	 }*/
-
-
-	@Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(
-			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-
-		BindingResult result = ex.getBindingResult();
-	    List<FieldError> fieldErrors = result.getFieldErrors();
-	    StringBuilder errorMessage = new StringBuilder();
-	   
-	    String messageDetail="error:{";
-        
-        int sizeFieldError = fieldErrors.size();
-        for (int it =0; it <  sizeFieldError; it++ ) {
-        	try {
-	        	FieldError f = fieldErrors.get(it);
-	        	String[] msgArr=(f.getDefaultMessage()).split("-");
-	        	List<String> msgList=Arrays.asList(msgArr);
-	        	String field=f.getField();
-	        	List<Object> param = new ArrayList<Object>();
-	        	param.addAll(msgList);
-	        	param.set(0, field);
-	        	String msg=msgArr[0];
-	        	if(msgList.get(0).startsWith("_")){
-	        		msg=messageSource.getMessage(msgList.get(0) ,null, 
-						   LocaleContextHolder.getLocale());
-	        		StringBuilder errorMessageR = new StringBuilder();
-	        		msgList= new ArrayList(Arrays.asList(msg.split("%s")));
-	        		msg="";
-	        		int k=0;
-	        		for (int j =0; j <  msgList.size(); j++ ) {
-	        			if (k<param.size()) {
-	        				msg= msg 
-	        					 + (j==0?"":" ")
-	        					 + msgList.get(j)
-	        					 + " "
-	        					 + param.get(k)
-	        					 + ((j+1)==msgList.size()?"":" ");
-	        				k++;
-	        			}
-	        			else
-	        				msg= msg
-	        					 + (j==0?"":" ")
-	        					 + msgList.get(j)
-	        					 + ((j+1)==msgList.size()?"":" ");
-	        		}
-	        		
-	        	}
-	        	msg= Util.cleanBlanks(msg);
-						   
-	        	messageDetail = messageDetail+f.getField() 
-	        			        + " : " 
-	        			        + msg
-	        			        + ( it== (sizeFieldError-1)? "" : ",");
-	        	LOGGER.info("Validation message ready");
-        	}catch(Exception ex1){
-        		LOGGER.warn(ex1.getMessage());
-        		 
-        	}
-        }
-        
-        messageDetail=messageDetail+"}";
-        messageDetail=Util.cleanBlanks(messageDetail);
-		ExceptionResponse exceptionResponse=
-				new ExceptionResponse( HttpStatus.BAD_REQUEST,new Date(),
-									   messageSource.getMessage("error.method_argument_not_valid_exception.message",null, 
-									   LocaleContextHolder.getLocale()),
-									   null,
-									   messageDetail);
-		
 		return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
 
-	 
-	
+	@ExceptionHandler(PersistenceException.class)
+	public final ResponseEntity<Object> handlePersistenceException(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.NOT_FOUND, new Date(),
+				"Error en campos de claves foraneas", request.getDescription(false), null);
+		return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public final ResponseEntity<Object> handleIllegalArgumentException(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR, new Date(),
+				ex.getMessage(), request.getDescription(false), null);
+		return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+
+	}
+
+	@ExceptionHandler(PredioNotFoundException.class)
+	public final ResponseEntity<Object> handleOfficerNotFoundException(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.NOT_FOUND, new Date(), ex.getMessage(),
+				request.getDescription(false), null);
+		return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+
+	}
+
+	@ExceptionHandler(com.fasterxml.jackson.core.JsonParseException.class)
+	public final ResponseEntity<Object> handleJsonParseException(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR, new Date(),
+				messageSource.getMessage("error.json_parse.exception.message", null, LocaleContextHolder.getLocale()),
+				ex.getMessage(), null);
+		return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		BindingResult result = ex.getBindingResult();
+		List<FieldError> fieldErrors = result.getFieldErrors();
+		StringBuilder errorMessage = new StringBuilder();
+
+		String messageDetail = "error:{";
+
+		int sizeFieldError = fieldErrors.size();
+		for (int it = 0; it < sizeFieldError; it++) {
+			try {
+				FieldError f = fieldErrors.get(it);
+				String[] msgArr = (f.getDefaultMessage()).split("-");
+				List<String> msgList = Arrays.asList(msgArr);
+				String field = f.getField();
+				List<Object> param = new ArrayList<Object>();
+				param.addAll(msgList);
+				param.set(0, field);
+				String msg = msgArr[0];
+				if (msgList.get(0).startsWith("_")) {
+					msg = messageSource.getMessage(msgList.get(0), null, LocaleContextHolder.getLocale());
+					StringBuilder errorMessageR = new StringBuilder();
+					msgList = new ArrayList(Arrays.asList(msg.split("%s")));
+					msg = "";
+					int k = 0;
+					for (int j = 0; j < msgList.size(); j++) {
+						if (k < param.size()) {
+							msg = msg + (j == 0 ? "" : " ") + msgList.get(j) + " " + param.get(k)
+									+ ((j + 1) == msgList.size() ? "" : " ");
+							k++;
+						} else
+							msg = msg + (j == 0 ? "" : " ") + msgList.get(j) + ((j + 1) == msgList.size() ? "" : " ");
+					}
+
+				}
+				msg = Util.cleanBlanks(msg);
+
+				messageDetail = messageDetail + f.getField() + " : " + msg + (it == (sizeFieldError - 1) ? "" : ",");
+				LOGGER.info("Validation message ready");
+			} catch (Exception ex1) {
+				LOGGER.warn(ex1.getMessage());
+
+			}
+		}
+
+		messageDetail = messageDetail + "}";
+		messageDetail = Util.cleanBlanks(messageDetail);
+		ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.BAD_REQUEST, new Date(), messageSource
+				.getMessage("error.method_argument_not_valid_exception.message", null, LocaleContextHolder.getLocale()),
+				null, messageDetail);
+
+		return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+
 }
--- src/main/java/ec/gob/mag/rna/predio/exception/ExceptionResponse.java
+++ src/main/java/ec/gob/mag/rna/predio/exception/ExceptionResponse.java
@@ -10,18 +10,14 @@ public class ExceptionResponse {
 	private String message;
 	private String details;
 	private String detailsJson;
-	
-	
-	
-	public ExceptionResponse(HttpStatus status, Date timestamp, String message, String details, String detailsJson) {
 
+	public ExceptionResponse(HttpStatus status, Date timestamp, String message, String details, String detailsJson) {
 		this.status = status;
 		this.timestamp = timestamp;
 		this.message = message;
 		this.details = details;
 		this.detailsJson = detailsJson;
 	}
-
 
 	public HttpStatus getStatus() {
 		return status;
@@ -35,29 +31,32 @@ public class ExceptionResponse {
 		return detailsJson;
 	}
 
-
 	public void setDetailsJson(String detailsJson) {
 		this.detailsJson = detailsJson;
 	}
 
-
 	public Date getTimestamp() {
 		return timestamp;
 	}
+
 	public void setTimestamp(Date timestamp) {
 		this.timestamp = timestamp;
 	}
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
+
 	public String getDetails() {
 		return details;
 	}
+
 	public void setDetails(String details) {
 		this.details = details;
 	}
-	
+
 }
