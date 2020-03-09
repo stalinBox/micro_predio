6590bc9f79842ed7c38f856cb46fb42aeb3797b2
Darwin Stalin Ramirez Supe <dramirez@mag.gob.ec>
2020-03-09 10:12:38 -0500

    refactorizado code

--- src/main/java/ec/gob/mag/rna/predio/MicroRnaPredio.java
+++ src/main/java/ec/gob/mag/rna/predio/MicroRnaPredio.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
